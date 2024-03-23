9302fd14fdecdf2bce8e2bab9cd3205ba743e116
dalf2598 <alejofon98@hotmail.com>
2024-03-23 10:17:28 -0500

    test: fix broken tests

--- src/components/organisms/PlayerForm/PlayerForm.test.tsx
+++ src/components/organisms/PlayerForm/PlayerForm.test.tsx
@@ -3,6 +3,11 @@ import { render, screen } from "@testing-library/react";
 import PlayerForm from "./PlayerForm";
 import { DIFFICULTY_LABEL, PLAYER_LABEL } from "../../../Constants";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: jest.fn(),
+}));
+
 describe("<RadioGroup />", () => {
   const setup = () => {
     return render(<PlayerForm />);
