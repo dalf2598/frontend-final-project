4876bd27d09db35d387cff545a956e7d91d864cb
dalf2598 <alejofon98@hotmail.com>
2024-03-23 16:31:52 -0500

    fix: fix broken tests

--- src/components/pages/Gameplay/Gameplay.test.tsx
+++ src/components/pages/Gameplay/Gameplay.test.tsx
@@ -1,16 +1,39 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { GameContextProps } from "../../../contexts/GameContext/GameContext.types";
+import { GameContext } from "../../../contexts/GameContext/GameContext";
 import Gameplay from "./Gameplay";
 
 describe("<Gameplay />", () => {
+  const mockGameContextProps: GameContextProps = {
+    playerName: "",
+    setPlayerName: () => {},
+    questions: [
+      {
+        category: "test category",
+        difficulty: "test difficulty",
+        query: "test query",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: "Option 1",
+      },
+    ],
+    setQuestions: () => {},
+    questionTimer: 0,
+    setQuestionTimer: () => {},
+  };
+
   const setup = () => {
-    return render(<Gameplay />);
+    return render(
+      <GameContext.Provider value={{ ...mockGameContextProps }}>
+        <Gameplay />
+      </GameContext.Provider>
+    );
   };
 
   test("should render child components", () => {
     setup();
 
-    const titleLabel = screen.getByText("Question");
+    const titleLabel = screen.getByText("Question 1");
 
     expect(titleLabel).toBeInTheDocument();
   });
