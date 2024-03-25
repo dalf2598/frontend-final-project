e4a5b6f76d338f446c18e3d497e0dce7191189d9
David Lopez <45211514+dalf2598@users.noreply.github.com>
2024-03-25 11:44:53 -0500

    test: created test for useLocalStorage hook (#26)

---
+++ src/hooks/useLocalStorage.test.tsx
@@ -0,0 +1,36 @@
+import { renderHook } from "@testing-library/react";
+import useLocalStorage from "./useLocalStorage";
+
+describe("useLocalStorage", () => {
+  beforeEach(() => {
+    Storage.prototype.setItem = jest.fn();
+    Storage.prototype.getItem = jest.fn();
+  });
+
+  it("writes to localStorage correctly", () => {
+    const { result } = renderHook(() => useLocalStorage());
+    const key = "testKey";
+    const value = { test: "data" };
+
+    result.current.writeLocalStorage(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      key,
+      JSON.stringify(value)
+    );
+  });
+
+  it("reads from localStorage correctly", () => {
+    const { result } = renderHook(() => useLocalStorage());
+    const key = "testKey";
+    result.current.readLocalStorage(key);
+    expect(localStorage.getItem).toHaveBeenCalledWith(key);
+  });
+
+  it("returns null when reading from non-existent key", () => {
+    const { result } = renderHook(() => useLocalStorage());
+    const key = "nonExistentKey";
+
+    expect(result.current.readLocalStorage(key)).toBeNull();
+  });
+});
