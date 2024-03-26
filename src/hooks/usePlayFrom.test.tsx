5dac4b0b9a19bc302cdf318c47c7032c9e61843a
David Lopez <45211514+dalf2598@users.noreply.github.com>
2024-03-25 20:23:53 -0500

    test: added test for usePlayerForm hook (#28)

---
+++ src/hooks/usePlayFrom.test.tsx
@@ -0,0 +1,49 @@
+import { act, renderHook } from "@testing-library/react";
+import usePlayerForm from "./usePlayerForm";
+import useFetchQuestions from "./useFetchQuestions";
+import { BackupQuestionBank } from "../backups/QuestionBank";
+
+jest.mock("../utils/Urls", () => ({
+  get API_URL() {
+    return "localhost:3000/api/questions";
+  },
+}));
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
+jest.mock("./useFetchQuestions");
+
+describe("usePlayFrom", () => {
+  const mockfetchQuestionBank = jest.fn().mockReturnValue(BackupQuestionBank);
+  const mockFetchQuestions = jest.mocked(useFetchQuestions);
+  mockFetchQuestions.mockReturnValue({
+    fetchQuestionBank: mockfetchQuestionBank,
+  });
+
+  test("should change the difficulty", () => {
+    const { result } = renderHook(() => usePlayerForm());
+
+    act(() => {
+      result.current.handleChangeDifficulty("easy");
+    });
+
+    expect(result.current.difficulty).toBe("easy");
+  });
+
+  test("should call appropiate function when clicking", () => {
+    const { result } = renderHook(() => usePlayerForm());
+
+    const mockPlayernName = "Player 1";
+    act(() => {
+      result.current.handlePlayButtonClick();
+      result.current.setPlayerName(mockPlayernName);
+      expect(mockfetchQuestionBank).toHaveBeenCalled();
+    });
+
+    expect(result.current.isDialogOpen).toBe(true);
+    expect(result.current.playerName).toBe(mockPlayernName);
+  });
+});
